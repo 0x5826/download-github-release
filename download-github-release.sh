c6ad3f3f50ebbb1396b6ba34ad1824cb07406f9b
Dante-Z <dante@0x5826.one>
2021-03-10 15:59:27 +0800

    Update download-github-release.sh

--- download-github-release.sh
+++ download-github-release.sh
@@ -6,11 +6,16 @@ function dl_lastest_release() {
     DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/$1/$2/releases/latest | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
     DOWNLOAD_FOLDER="$1-$2-$VERSION"
     if [ -d "$DOWNLOAD_FOLDER" ];then
-    	echo "[Notice]Found folder $DOWNLOAD_FOLDER, Do nothing."
+        echo -e "\033[33m[Notice]\033[0mFound folder $DOWNLOAD_FOLDER, Do nothing."
     else
-    	echo "[Notice]Creating folder $DOWNLOAD_FOLDER"
-    	mkdir $DOWNLOAD_FOLDER && cd $DOWNLOAD_FOLDER
-    	echo "$DOWNLOAD_URL" | grep $VERSION | xargs wget -q --show-progress
+        echo -e "\033[33m[Notice]\033[0mCreating folder $DOWNLOAD_FOLDER"
+        mkdir $DOWNLOAD_FOLDER && cd $DOWNLOAD_FOLDER
+        for DOWNLOAD_FILE in $DOWNLOAD_URL
+        do
+            echo -e "\033[33m[Notice]\033[0mDownloading \033[32m$DOWNLOAD_FILE\033[0m"
+            curl -O -fSL -# $DOWNLOAD_FILE
+        done
+        
     fi
 }
 
@@ -18,34 +23,38 @@ function dl_custom_release() {
     DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/$1/$2/releases/tags/$3 | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
     DOWNLOAD_FOLDER="$1-$2-$3"
     if [ -d "$DOWNLOAD_FOLDER" ];then
-    	echo "[Notice]Found folder $DOWNLOAD_FOLDER, Do nothing."
+        echo -e "\033[33m[Notice]\033[0mFound folder $DOWNLOAD_FOLDER, Do nothing."
     else
-    	echo "[Notice]Creating folder $DOWNLOAD_FOLDER"
-    	mkdir $DOWNLOAD_FOLDER && cd $DOWNLOAD_FOLDER
-    	wget -q --show-progress $DOWNLOAD_URL
+        echo -e "\033[33m[Notice]\033[0mCreating folder $DOWNLOAD_FOLDER"
+        mkdir $DOWNLOAD_FOLDER && cd $DOWNLOAD_FOLDER
+        for DOWNLOAD_FILE in $DOWNLOAD_URL
+        do
+            echo -e "\033[33m[Notice]\033[0mDownloading \033[32m$DOWNLOAD_FILE\033[0m"
+            curl -O -fSL -# $DOWNLOAD_FILE
+        done
     fi
 }
 
 function help_info() {
-	echo -e "usage:\n ./download-github-release.sh latest v2fly v2ray-core\n ./download-github-release.sh custom v2fly v2ray-core v1.6.0-rc5"
+    echo -e "usage:\n ./download-github-release.sh latest v2fly v2ray-core\n ./download-github-release.sh custom v2fly v2ray-core v1.6.0-rc5"
 }
 
 if [ "$1" = "latest" ];then
-	if [[ -n "$2" ]] && [[ -n "$3" ]];then
-    	dl_lastest_release $2 $3
+    if [[ -n "$2" ]] && [[ -n "$3" ]];then
+        dl_lastest_release $2 $3
     else
-    	help_info
+        help_info
     fi
 elif [ "$1" = "custom" ];then
-	if [[ -n "$2" ]] && [[ -n "$3" ]];then
-    	if [ -n "$4"  ];then
-    		dl_custom_release $2 $3 $4
+    if [[ -n "$2" ]] && [[ -n "$3" ]];then
+        if [ -n "$4"  ];then
+            dl_custom_release $2 $3 $4
         else
-        	help_info
+            help_info
         fi
     else
-    	help_info
+        help_info
     fi
 else
-	help_info
+    help_info
 fi
