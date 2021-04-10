fdcadea5fe7769df20a3bcec37ba99534e4834ae
0x5826 <dante@0x5826.one>
2021-04-10 12:28:12 +0800

    add download all releases

--- download-github-release.sh
+++ download-github-release.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#proxy="-x socks5://127.0.0.1:1080"
+proxy=""
+
 #download the release
 function dl_lastest_release() {
-    VERSION=$(curl -fsSL https://api.github.com/repos/$1/$2/releases/latest | grep tag_name | sed -E 's/.*"(.*)".*/\1/')
-    DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/$1/$2/releases/latest | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
-    DOWNLOAD_FOLDER="$1-$2-$VERSION"
+    VERSION=$(curl -fsSL $proxy https://api.github.com/repos/$1/$2/releases/latest | grep tag_name | sed -E 's/.*"(.*)".*/\1/')
+    DOWNLOAD_URL=$(curl -fsSL $proxy https://api.github.com/repos/$1/$2/releases/latest | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
+    DOWNLOAD_FOLDER="releases-$1-$2-$VERSION"
     if [ -d "$DOWNLOAD_FOLDER" ];then
         echo -e "\033[33m[Notice]\033[0mFound folder $DOWNLOAD_FOLDER, Do nothing."
     else
@@ -13,15 +16,15 @@ function dl_lastest_release() {
         for DOWNLOAD_FILE in $DOWNLOAD_URL
         do
             echo -e "\033[33m[Notice]\033[0mDownloading \033[32m$DOWNLOAD_FILE\033[0m"
-            curl -O -fSL -# $DOWNLOAD_FILE
+            curl -O -fSL $proxy -# $DOWNLOAD_FILE
         done
         
     fi
 }
 
 function dl_custom_release() {
-    DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/$1/$2/releases/tags/$3 | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
-    DOWNLOAD_FOLDER="$1-$2-$3"
+    DOWNLOAD_URL=$(curl -fsSL $proxy https://api.github.com/repos/$1/$2/releases/tags/$3 | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
+    DOWNLOAD_FOLDER="releases-$1-$2-$3"
     if [ -d "$DOWNLOAD_FOLDER" ];then
         echo -e "\033[33m[Notice]\033[0mFound folder $DOWNLOAD_FOLDER, Do nothing."
     else
@@ -30,7 +33,30 @@ function dl_custom_release() {
         for DOWNLOAD_FILE in $DOWNLOAD_URL
         do
             echo -e "\033[33m[Notice]\033[0mDownloading \033[32m$DOWNLOAD_FILE\033[0m"
-            curl -O -fSL -# $DOWNLOAD_FILE
+            curl -O -fSL $proxy -# $DOWNLOAD_FILE
+        done
+    fi
+}
+
+function dl_all_release() {
+    VERSIONS=$(curl -fsSL $proxy https://api.github.com/repos/$1/$2/releases | grep tag_name | sed -E 's/.*"(.*)".*/\1/')
+    DOWNLOAD_FOLDER="releases-$1-$2"
+    if [ -d "$DOWNLOAD_FOLDER" ];then
+        echo -e "\033[33m[Notice]\033[0mFound folder $DOWNLOAD_FOLDER."
+    else
+        echo -e "\033[33m[Notice]\033[0mCreating folder $DOWNLOAD_FOLDER."
+        mkdir $DOWNLOAD_FOLDER && cd $DOWNLOAD_FOLDER
+        for VERSION in $VERSIONS
+        do
+            echo -e "\033[33m[Notice]\033[0mCreating folder $DOWNLOAD_FOLDER/$VERSION"
+            mkdir $VERSION && cd $VERSION
+            DOWNLOAD_URL=$(curl -fsSL $proxy https://api.github.com/repos/$1/$2/releases/tags/$VERSION | grep browser_download_url | sed -E 's/.*"(.*)".*/\1/')
+            for DOWNLOAD_FILE in $DOWNLOAD_URL
+            do
+                echo -e "\033[33m[Notice]\033[0mDownloading \033[32m$DOWNLOAD_FILE\033[0m"
+                curl -O -fSL $proxy -# $DOWNLOAD_FILE
+            done
+            cd ..
         done
     fi
 }
@@ -42,6 +68,12 @@ function help_info() {
 if [ "$1" = "latest" ];then
     if [[ -n "$2" ]] && [[ -n "$3" ]];then
         dl_lastest_release $2 $3
+    else
+        help_info
+    fi
+elif [ "$1" = "all" ];then
+    if [[ -n "$2" ]] && [[ -n "$3" ]];then
+        dl_all_release $2 $3
     else
         help_info
     fi
